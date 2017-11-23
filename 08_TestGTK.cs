7b0b27652b9388e107d20cef4845caecea7c2193
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-11-23 22:36:46 +0700

    Add 08 test gtk, not yet running, it seems that gtk-sharp is not installed

---
+++ 08_TestGTK.cs
@@ -0,0 +1,14 @@
+using Gtk;
+using System;
+
+class Hello
+{
+  static void Main ()
+  {
+    Application.Init ();
+    Window window = new Window ("Hello Mono World from ffr");
+    window.Show ();
+    Application.Run ();
+   }
+}
+
